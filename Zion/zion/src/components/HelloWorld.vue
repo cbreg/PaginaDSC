09b05501f761ed28da6c3a4c6adfcf6e4a8b6028
Caroline <carol_regalin@hotmail.com>
2019-04-21 19:36:16 -0300

    Base login

--- Zion/zion/src/components/HelloWorld.vue
+++ Zion/zion/src/components/HelloWorld.vue
@@ -1,85 +1,36 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="centered-container">
+    <md-content class="md-elevation-3">
+
+      <div class="title">
+        <img src="https://vuematerial.io/assets/logo-color.png">
+        <div class="md-title">Vue Material</div>
+        <div class="md-body-1">Build beautiful apps with Material Design and Vue.js</div>
+      </div>
+
+      <div class="form">
+        <md-field>
+          <label>E-mail</label>
+          <md-input v-model="login.email" autofocus></md-input>
+        </md-field>
+
+        <md-field md-has-password>
+          <label>Password</label>
+          <md-input v-model="login.password" type="password"></md-input>
+        </md-field>
+      </div>
+
+      <div class="actions md-layout md-alignment-center-space-between">
+        <a href="/resetpassword">Reset password</a>
+        <md-button class="md-raised md-primary" @click="auth">Log in</md-button>
+      </div>
+
+      <div class="loading-overlay" v-if="loading">
+        <md-progress-spinner md-mode="indeterminate" :md-stroke="2"></md-progress-spinner>
+      </div>
+
+    </md-content>
+    <div class="background" />
   </div>
 </template>
 
@@ -94,20 +45,3 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- Zion/zion/src/main.js
+++ Zion/zion/src/main.js
@@ -2,20 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.css'
-Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
-
-Vue.material.theme.register('default', {
-  primary: 'light-blue',
-  accent: 'pink'
 })
